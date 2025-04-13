4cb79c1423cdc4a9d1bf63e2d8ebeb669550f8a5
Afshan Martin <afshan.aman@gmail.com>
2025-04-13 12:19:11 -0600

    first commit

---
+++ lib/Basic.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps({
+      msg: { type: String }
+    }
+)
+
+const count = ref(0)
+</script>
